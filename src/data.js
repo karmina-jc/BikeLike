cdd158e55cde60013c52c17ca51451a3c8b1dac2
karmina-jc <karmina.jarac@gmail.com>
2020-03-16 02:01:37 -0300

    actuallizacion app mini desafio here

--- src/data.js
+++ src/data.js
@@ -1,115 +1,183 @@
-// /* Manejo de data */
+// Initialize the platform object:
+const platform = new H.service.Platform({
+  'apikey': 'luAvmoHbQUvxSVLucOwLZlrXOQ9JvIjUWuYPjqU1nsY'
+   });
+   
+// Obtain the default map types from the platform object
+const maptypes = platform.createDefaultLayers();
 
-// // esta es una función de ejemplo
+// Instantiate (and display) a map object:
+const map = new H.Map(
+   document.getElementById('root'),
+   maptypes.vector.normal.map,
+   {
+   zoom: 11,
+   center: { lat: -33.44, lng: -70.66 }
+   });
 
-// export const example = () => {
-//   return 'example';
-// };
-  const startValue = {
-    searchText: document.getElementById('Desde').value
-  };
+// Enable the event system on the map instance:
+const mapEvents = new H.mapevents.MapEvents(map);
 
-  const finishValue = {
-    searchText: document.getElementById('Hasta').value
-  };
+// Add event listener:
+map.addEventListener('tap', function(evt) {
+   // Log 'tap' and 'mouse' events:
+   console.log(evt.type, evt.currentPointer.type); 
+});
+const behavior = new H.mapevents.Behavior(mapEvents);
+const ui = H.ui.UI.createDefault(map, maptypes); 
 
+//value for decoding
+const geocoderService = platform.getGeocodingService();
+const geocoder = adress =>{
+  return new Promise ((resolve, reject) => {
+    geocoderService.geocode(
+      {
+        'searchtext': adress
+      },
+      success => {
+        resolve(success.Response.View[0].Result[0].Location.DisplayPosition);
+      },
+      error => {
+        reject(error)
+      }
+    )
+  })
+}
 
-  // Define a callback function to process the geocoding response:
-  const convertPoint = function(result) {
-  let locations = result.Response.View[0].Result,
-    position,
-    marker;
-  // Add a marker for each location found
-  for (let i = 0;  i < locations.length; i++) {
-    position = {
-      lat: locations[i].Location.DisplayPosition.Latitude,
-      lng: locations[i].Location.DisplayPosition.Longitude
-    };
-    marker = new H.map.Marker(position);
-    map.addObject(marker);
-    }
-  };
-
-  // Get an instance of the geocoding service:
-  const geocoder = platform.getGeocodingService();
-
-  geocoder.geocode(startValue, convertPoint, function(e) {
-  alert(e);
-  });
-
-  geocoder.geocode(finishValue, convertPoint, function(e) {
-  alert(e);
-  });
-
+const userRouting = async (startAdress, finishAdress) => {
+  const startPiont = await geocoder(startAdress);
+  const finishPoint = await geocoder(finishAdress)
+  
+  // Create the parameters for the routing request:
   const routingParameters = {
     // The routing mode:
-    'mode': 'fastest;pedestrian',
+    'mode': 'fastest;bicycle',
     // The start point of the route:
-    'waypoint0': `geo!${position.lat},${position.lng}`,
+    'waypoint0': `geo!${startPiont.Latitude},${startPiont.Longitude}`,
     // The end point of the route:
-    'waypoint1': `geo!${position.lat},${position.lng}`,
+    'waypoint1': `geo!${finishPoint.Latitude},${finishPoint.Longitude}`,
     // To retrieve the shape of the route we choose the route
     // representation mode 'display'
     'representation': 'display'
   };
-  
-  // Define a callback function to process the routing response:
-  const ruteOp = function(result) {
-    let route,
+
+// Define a callback function to process the routing response:
+  const onResult = function(result) {
+    var route,
       routeShape,
       startPoint,
       endPoint,
       linestring;
-      
     if(result.response.route) {
     // Pick the first route from the response:
     route = result.response.route[0];
     // Pick the route's shape:
     routeShape = route.shape;
-  
+
     // Create a linestring to use as a point source for the route line
     linestring = new H.geo.LineString();
-  
+
     // Push all the points in the shape into the linestring:
     routeShape.forEach(function(point) {
-      let parts = point.split(',');
+      var parts = point.split(',');
       linestring.pushLatLngAlt(parts[0], parts[1]);
     });
-  
+
     // Retrieve the mapped positions of the requested waypoints:
     startPoint = route.waypoint[0].mappedPosition;
     endPoint = route.waypoint[1].mappedPosition;
-  
+
     // Create a polyline to display the route:
     const routeLine = new H.map.Polyline(linestring, {
       style: { strokeColor: 'blue', lineWidth: 3 }
     });
-  
+
+   document.querySelectorAll('.userOption button').forEach(e => {
+      e.addEventListener('click', () => {
+        console.log(e.value)
+
+        if(e.value === 'like'){
+          
+          routeLine.addEventListener('tap', (event) => {
+            let position = map.screenToGeo(
+              event.currentPointer.viewportX,
+              event.currentPointer.viewportY
+            )
+            let like = new H.map.Icon('./hearticon.png');
+            const marker = new H.map.Marker(position,{icon:like})
+            map.addObject(marker)
+          })        
+          
+        }else if (e.value === 'hole'){
+          
+          routeLine.addEventListener('tap', (event) => {
+            let position = map.screenToGeo(
+              event.currentPointer.viewportX,
+              event.currentPointer.viewportY
+            )
+            let hole = new H.map.Icon('./holeicon.png');
+            const marker = new H.map.Marker(position,{icon:hole})
+            map.addObject(marker)
+          })
+        }else if (e.value === 'danger'){
+          
+          routeLine.addEventListener('tap', (event) => {
+            let position = map.screenToGeo(
+            event.currentPointer.viewportX,
+            event.currentPointer.viewportY
+           )
+          let danger = new H.map.Icon('./warningicon.png')
+          const marker = new H.map.Marker(position,{icon:danger})
+          map.addObject(marker)
+          }) 
+        }else{
+          alert('Selecciona el punto de la ruta donde deseas poner tu pin')
+        }
+      })
+
+    })
+
     // Create a marker for the start point:
     let startMarker = new H.map.Marker({
       lat: startPoint.latitude,
       lng: startPoint.longitude
     });
-  
+
     // Create a marker for the end point:
     let endMarker = new H.map.Marker({
       lat: endPoint.latitude,
       lng: endPoint.longitude
     });
-  
+
     // Add the route polyline and the two markers to the map:
     map.addObjects([routeLine, startMarker, endMarker]);
-  
+
     // Set the map's viewport to make the whole route visible:
     map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
     }
   };
-  
+
   // Get an instance of the routing service:
   var router = platform.getRoutingService();
-  
-  router.calculateRoute(routingParameters, ruteOp,
+
+  // Call calculateRoute() with the routing parameters,
+  // the callback and an error callback function (called if a
+  // communication error occurs):
+  router.calculateRoute(routingParameters, onResult,
     function(error) {
       alert(error.message);
     });
-  
+}
+
+const btnRute = document.getElementById('Ruta')
+
+btnRute.addEventListener('click', () => {
+  let startAdress = document.getElementById('Desde').value;
+  let finishAdress = document.getElementById('Hasta').value
+  userRouting(startAdress, finishAdress)
+})
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 const platform = new H.service.Platform({
    'apikey': 'luAvmoHbQUvxSVLucOwLZlrXOQ9JvIjUWuYPjqU1nsY'
     });
-
     
 // Obtain the default map types from the platform object
 const maptypes = platform.createDefaultLayers();
@@ -25,78 +24,118 @@ map.addEventListener('tap', function(evt) {
     console.log(evt.type, evt.currentPointer.type); 
 });
 const behavior = new H.mapevents.Behavior(mapEvents);
-
 const ui = H.ui.UI.createDefault(map, maptypes);
-
 const btnRuta = document.getElementById('Ruta')
 
 btnRuta.addEventListener('click', () => {
  
+  const startValue = {
+    searchText: document.getElementById('Desde').value
+  };
   const finishValue = {
     searchText: document.getElementById('Hasta').value
   };
-
+   
   // Define a callback function to process the geocoding response:
   const convertPoint = function(result) {
   let locations = result.Response.View[0].Result,
-    position,
-    marker;
-   
+    position
+
   // Add a marker for each location found
   for (let i = 0;  i < locations.length; i++) {
     position = {
       lat: locations[i].Location.DisplayPosition.Latitude,
       lng: locations[i].Location.DisplayPosition.Longitude
     };
+    console.log(position)
+    }
+  const routingParameters = {
 
-
-    marker = new H.map.Marker(position);
-    map.addObject(marker);
+    // The routing mode:
+    'mode': 'fastest;bicycle',
+    // The start point of the route:
+    'waypoint0': `geo!${position.lat},${position.lng}`,
+    // The end point of the route:
+    'waypoint1': `geo!${position.lat},${position.lng}`,
+    // To retrieve the shape of the route we choose the route
+    // representation mode 'display'
+    'representation': 'display'
+  };
+    
+  var onResult = function(result) {
+    var route,
+      routeShape,
+      startPoint,
+      endPoint,
+      linestring;
+    if(result.response.route) {
+    // Pick the first route from the response:
+    route = result.response.route[0];
+    // Pick the route's shape:
+    routeShape = route.shape;
+  
+    // Create a linestring to use as a point source for the route line
+    linestring = new H.geo.LineString();
+  
+    // Push all the points in the shape into the linestring:
+    routeShape.forEach(function(point) {
+      var parts = point.split(',');
+      linestring.pushLatLngAlt(parts[0], parts[1]);
+    });
+  
+    // Retrieve the mapped positions of the requested waypoints:
+    startPoint = route.waypoint[0].mappedPosition;
+    endPoint = route.waypoint[1].mappedPosition;
+  
+    // Create a polyline to display the route:
+    var routeLine = new H.map.Polyline(linestring, {
+      style: { strokeColor: 'blue', lineWidth: 3 }
+    });
+  
+    // Create a marker for the start point:
+    var startMarker = new H.map.Marker({
+      lat: startPoint.latitude,
+      lng: startPoint.longitude
+    });
+  
+    // Create a marker for the end point:
+    var endMarker = new H.map.Marker({
+      lat: endPoint.latitude,
+      lng: endPoint.longitude
+    });
+  
+    // Add the route polyline and the two markers to the map:
+    map.addObjects([routeLine, startMarker, endMarker]);
+  
+    // Set the map's viewport to make the whole route visible:
+    map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
     }
   };
+  
+  // Get an instance of the routing service:
+  var router = platform.getRoutingService();
+  
+  // Call calculateRoute() with the routing parameters,
+  // the callback and an error callback function (called if a
+  // communication error occurs):
+  router.calculateRoute(routingParameters, onResult,
+    function(error) {
+      alert(error.message);
+    });
+  };
 
-   // Get an instance of the geocoding service:
+  // Get an instance of the geocoding service:
   const geocoder = platform.getGeocodingService();
-
+  
+  geocoder.geocode(startValue, convertPoint, function(e) {
+  alert(e);
+  });
+ 
   geocoder.geocode(finishValue, convertPoint, function(e) {
   alert(e);
   });
-  
-  const city = document.getElementById('Ciudad').value
+})
 
-  fetch(`https://weather.ls.hereapi.com/weather/1.0/report.json?apiKey=luAvmoHbQUvxSVLucOwLZlrXOQ9JvIjUWuYPjqU1nsY&product=observation&name=${city}`)
-  .then(res => res.json())
-  .then(data => {
-    let weatherData = data.observations.location
-    for (let i = 0; i < weatherData.length; i++) {
-      let position = {
-        lat: weatherData[i].latitude,
-        lng: weatherData[i].longitude
-      };
-      console.log(weatherData[i].observation[i].skyDescription)
-      if(weatherData[i].observation[i].highTemperature >= 28){
-        // Create an info bubble object at a specific geographic location:
-        let bubble = new H.ui.InfoBubble(position, {
-        content: '<b>Dia caluroso, recuerda tu agua y bloqueador</b>'
-        });
-        // Add info bubble to the UI:
-        ui.addBubble(bubble);
-      }else if (weatherData[i].observation[i].highTemperature <10){
-        // Create an info bubble object at a specific geographic location:
-        let bubble = new H.ui.InfoBubble(position, {
-        content: '<b>Dia frio, no olvides salir abrigado</b>'
-        });
-        ui.addBubble(bubble);
-      } else {
-          // Create an info bubble object at a specific geographic location:
-          let bubble = new H.ui.InfoBubble(position, {
-          content: '<b>Dia agradeble</b>'
-          });
-          ui.addBubble(bubble);
-      }
-  }
-})
-})
 
 /*fetch('https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey=luAvmoHbQUvxSVLucOwLZlrXOQ9JvIjUWuYPjqU1nsY&waypoint0=geo!52.5,13.4&waypoint1=geo!52.5,13.45&mode=fastest;bicycle;traffic:disabled')
 .then(res => res.json())
